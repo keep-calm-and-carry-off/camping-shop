5aecbdbba67c4645730a78929f948e8cf5f63c3d
Andrey Polyakov <skiper94@ya.ru>
2025-02-13 21:04:57 +0300

    убрал проверку коннекта с бд

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -2,27 +2,10 @@ import { useEffect } from 'react';
 import { Counter } from './Counter'
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
-
 export { Page }
 
 function Page() {
-  useEffect(() => {
-    async function checkConnection() {
-      try {
-        const { data, error } = await supabase.from('products').select('*').limit(1);
-    
-        if (error) {
-          console.error('Ошибка подключения:', error.message);
-        } else {
-          console.log('Подключение успешно! Получены данные:', data);
-        }
-      } catch (err) {
-        console.error('Произошла ошибка:', err);
-      }
-    }
-    checkConnection();
-  }, [])
+  
   return (
     <>
       <h1>Welcome</h1>
